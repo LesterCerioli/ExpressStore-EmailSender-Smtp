5ae94a3ca4762a85398f23ccb96a6426d900ec06
LesterCerioli <lesterlucasit@hotmail.com>
2022-10-20 16:42:44 -0300

    feat(#250): building Entities into infrastructure project

---
+++ test/ExpressStore.Modules.EmailSender.Smtp.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ExpressStore.Modules.EmailSender.Smtp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ test/ExpressStpre.EmailSender.Smtp.Infrastructure.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ExpressStpre.EmailSender.Smtp.Infrastructure.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
